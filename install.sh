a5bff463a990baf149110cdabb65fffadf0e110b
Federico Bond <federicobond@gmail.com>
2025-11-05 16:48:43 +1100

    Update config files

--- install.sh
+++ install.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-git clone --recursive https://github.com/zimfw/zimfw.git ${ZDOTDIR:-${HOME}}/.zim
-
-setopt EXTENDED_GLOB
-for template_file ( ${ZDOTDIR:-${HOME}}/.zim/templates/* ); do
-  user_file="${ZDOTDIR:-${HOME}}/.${template_file:t}"
-  touch ${user_file}
-  ( print -rn "$(<${template_file})$(<${user_file})" >! ${user_file} ) 2>/dev/null
-done
-
-echo "Initializing submodules"
-git submodule init
-git submodule update
 
 echo "Deleting the old files"
 rm -f ~/.profile
